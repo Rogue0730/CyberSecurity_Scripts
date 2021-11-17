db1bd8684f567e30c2c55d30585ab23d2e6c9d46
Keegan McCarthy <70453256+Rogue0730@users.noreply.github.com>
2021-11-16 18:25:32 -0600

    Update main.bat

--- MHSCybersecurityScript/main.bat
+++ MHSCybersecurityScript/main.bat
@@ -1,10 +1,36 @@
 @ECHO OFF
 
-CHOICE /c doStart /m Enter "1" to start:
+SET /P doStart=Enter 1 to start automation: 
+IF %doStart%==1 GOTO :startAutomation 
+GOTO :end
 
-IF ERRORLEVEL == 1 goto StartAutomation
 
-:StartAutomation
-echo Starting Automation . . .
-Rem -- vv End of Function vv --
-exit /b
+:startAutomation
+echo Starting . . .
+cls
+
+netsh advfirewall set allprofiles state on
+echo Enabled Firewall
+
+net accounts /MINPWLEN:12
+net accounts /MAXPWAGE:90
+net accounts /MINPWAGE:10
+net accounts /UNIQUEPW:10
+echo Password Policies Completed
+
+net accounts /lockoutthreshold:10
+echo Account Lockout Policies Completed
+
+net stop msftpsvc
+echo Stopped Unnessacry Services
+
+cd C:\
+del /S /Q C:\*.mp3
+del /S /Q C:\*.mp4
+echo Remove All Media Files
+GOTO :end
+
+:end
+echo Please close this window . . . 
+
+pause
