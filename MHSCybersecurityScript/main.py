b1aa0e154205b4b97b9367af55b66bf78fbdab3b
Keegan McCarthy <70453256+Rogue0730@users.noreply.github.com>
2021-11-18 10:32:15 -0600

    Update main.py

--- MHSCybersecurityScript/main.py
+++ MHSCybersecurityScript/main.py
@@ -1,18 +1,27 @@
-import os
-
 # Stores the admins
-admins[]
+admins = []
 
-# Asks user for admin account
-admin = input("Input all of the admins listed in the README (use commas to seperate each user, no spaces!): ")
-admin += ','
-
-# Loops through the admins and adds them to the admin list
-adminCache = ""
-for i in len(admin):
-    if i != ",":
-        adminCache += i
-    elif i == ",":
-        admins.append(adminCache)
-        adminCache = ""
-
+# Function to get the authorized admins in README file
+def userGroup_ADMINS():
+  # Asks user for admin account
+  adminInput = input("Input all of the admins listed in the README (Use commas to seperate each user, no spaces!): ")
+  adminInput += ','
+  
+  adminCache = ""
+  # Loops through the admins and adds them to the admin list
+  while True:
+    for i in range(len(adminInput)):
+      if adminInput[i] != ",":
+          adminCache += adminInput[i]
+      elif adminInput[i] == ",":
+          admins.append(adminCache)
+          adminCache = ""
+    # Asks the user if the admins collected are corrects
+    print(admins_
+    isAdminsCorrect = input('Is this correct? (Y/N): ')
+    if isAdminsCorrect = 'Y':
+      break
+    elif isAdminsCorrect == 'N':
+      continue
+    else: 
+      continue
