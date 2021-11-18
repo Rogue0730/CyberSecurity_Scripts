c42bc31f31fad19af77d1485d35d6bbe1b8e4463
Keegan McCarthy <70453256+Rogue0730@users.noreply.github.com>
2021-11-18 08:13:57 -0600

    Update main.py

--- MHSCybersecurityScript/main.py
+++ MHSCybersecurityScript/main.py
@@ -1,1 +1,18 @@
+import os
+
+# Stores the admins
+admins[]
+
+# Asks user for admin account
+admin = input("Input all of the admins listed in the README (use commas to seperate each user, no spaces!): ")
+admin += ','
+
+# Loops through the admins and adds them to the admin list
+adminCache = ""
+for i in len(admin):
+    if i != ",":
+        adminCache += i
+    elif i == ",":
+        admins.append(adminCache)
+        adminCache = ""
 
